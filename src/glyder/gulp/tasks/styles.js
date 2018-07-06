523e5639821e99959db86f082f7f43420c043741
Michael Lee <michaelsoolee@gmail.com>
2018-07-06 11:03:49 -0400

    Clean up

--- src/glyder/gulp/tasks/styles.js
+++ src/glyder/gulp/tasks/styles.js
@@ -7,9 +7,6 @@ import gulpLoadPlugins from 'gulp-load-plugins';
 const $ = gulpLoadPlugins();
 gulp.task('styles', () => {
   var building = process.env.build === 'true';
-  console.log(config.src.glob('styles'))
-  console.log(config.tmp.path('styles'))
-  console.log(process.cwd())
 
   return gulp.src(config.src.glob('styles'))
     .pipe($.plumber())
